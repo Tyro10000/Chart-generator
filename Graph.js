2b8d9f868ac3bb7ddac53151d81a804cec0be268
Tyro10000 <tmcelrat@kent.edu>
2016-11-04 08:12:46 -0400

    change ui

--- Graph.js
+++ Graph.js
@@ -1,10 +1,8 @@
 var sheet= document.getElementById("pie-chart");
 var ctx = sheet.getContext("2d");
-ctx.font = "15px Verdana"; 
+ctx.font = "12px Verdana"; 
 
 ctx.fillStyle = "darkblue";
-
-
 function pieChart(){
     var data = document.getElementById("slices").value;
     var names = document.getElementById("names").value;
@@ -13,11 +11,11 @@ function pieChart(){
     //alert(data.length);
     
     var colors = ["blue","red","orange","skyblue","darkblue","green","gray"];
-    var center = [200,150];
-    var radius = Math.min(250,250) / 2;
+    var center = [125,100];
+    var radius = Math.min(170,170) / 2;
     var lastPosition = 1.2;
     var total = 0;
-    var namePos = 100;
+    var namePos = 20;
     ctx.clearRect(0,0,sheet.width,sheet.height);
     for (var i = 0; i < data.length; i++) {
     total = total + parseInt(data[i]);
@@ -26,14 +24,36 @@ function pieChart(){
         ctx.fillStyle = colors[i];
         var percent = (data[i]*100)/total;
         percent  = percent.toFixed(2);
-        ctx.fillRect(380,namePos-10,20,20);
-        ctx.fillText(names[i] + " " + percent + "%",410,namePos);
+        ctx.fillRect(225,namePos-10,15,15);
+        ctx.fillText(names[i] + " " + percent + "%",250,namePos);
         ctx.beginPath();
         ctx.moveTo(center[0],center[1]);
         ctx.arc(center[0],center[1],radius,lastPosition,lastPosition+(Math.PI*2*(data[i]/total)));
         ctx.lineTo(center[0],center[1]);
         ctx.fill();
         lastPosition += Math.PI*2*(data[i]/total);
-        namePos += 30;
+        namePos += 20;
     }
 }
+function Clean(){
+    document.getElementById("slices").value = "";
+    document.getElementById("names").value = "";
+}
+function Post(name, type) {
+    var data = document.getElementById("slices").value;
+    var names = document.getElementById("names").value;
+    var data = data.split(",");
+    var names = names.split(",");
+    var text = "";
+    for(var i = 0; i < data.length; i++) {
+        text += names[i] + "-" + data[i] + ":";
+    }
+  var a = document.getElementById("link");
+  var file = new Blob([text], {type: type});
+  var url = "data:text/plain;charset=utf-8," + "Push";
+  a.href = url;
+  //a.href = URL.createObjectURL(file);
+  a.download = name;
+}
+
+
